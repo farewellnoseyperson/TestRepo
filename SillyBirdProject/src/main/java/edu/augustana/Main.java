aebb9ab93de9a54ed71a5a218d0e12aa5b5bf768
Noah Farwell <noahfarwell22@augustana.edu>
2024-09-24 14:09:50 -0500

    Now with dancing chicken

--- SillyBirdProject/src/main/java/edu/augustana/Main.java
+++ SillyBirdProject/src/main/java/edu/augustana/Main.java
@@ -1,7 +1,14 @@
 package edu.augustana;
 
 public class Main {
+
     public static void main(String[] args) {
+
+        Chicken chicken = new Chicken();
+
         System.out.println("Hello bird world!");
+
+        chicken.dance();
+
     }
 }
